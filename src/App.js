6a892ecc673337732747f73485770e3e07d248ff
Aranda <xavier.aranda@dxc.com>
2023-03-14 11:05:11 +0100

    Martes 14/03/23: Finalizado.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link, Route } from "wouter";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
 
 function App() {
-
-
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword='panda' />
+        <h1>Giphy API</h1>
+        <header>
+          <Link to="/gif/Panda">Gifs de pandas</Link>
+          <Link to="/gif/Rick">Gifs de Rick</Link>
+          <Link to="/gif/Morty">Gifs de Morty</Link>
+          <Link to="/gif/Bart">Gifs de Bart</Link>
+        </header>
+        <Route component={ListOfGifs} path="/gif/:keyword" />
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,15 +2,34 @@ import React, { useState, useEffect } from "react";
 import Gif from "./Gif";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ keyword }) {
+export default function ListOfGifs({ params }) {
+  const { keyword } = params;
+
+  const [loading, setLoading] = useState(true )
   const [gifs, setGifs] = useState([]);
 
-  useEffect(function () {
-    console.log("efecto ejecutado");
-    getGifs({ keyword }).then((gifs) => setGifs(gifs));
-  }, [keyword]);
+  useEffect(
+    
+    function () {
+      setLoading(true)
+      getGifs({ keyword }).then((gifs) => {
+        setGifs(gifs)
+        setLoading(false);
+      });
+      
+    },
+    [keyword]
+  );
 
-  return gifs.map(({ id, title, url }) => (
-    <Gif key={id} title={title} url={url} id={id} />
-  ));
+  if (loading) return <p>Cargando...</p>
+
+  return (
+    <>
+      {
+      gifs.map(({ id, title, url }) => (
+        <Gif key={id} title={title} url={url} id={id} />
+      ))
+      }
+    </>
+  );
 }
